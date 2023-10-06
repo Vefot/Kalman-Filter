6220f544e0491aba4a866f54076fc72b7c571fb6
Vefot <benjamin200@hotmail.co.uk>
2023-10-06 17:32:44 +0100

    comments

--- kf.py
+++ kf.py
@@ -16,7 +16,7 @@ class KF:
         self._x = np.array([initial_x, initial_v])
         self._accel_variance = accel_variance
 
-        # covariance of state gaussian random variable
+        # covariance matrix - represents uncertainty in the state
         self._P = np.eye(2)  # eye returns a 2x2 identity matrix
 
     def predict(self, dt: float) -> None:
@@ -25,22 +25,27 @@ class KF:
         """
         # x = F * x
         # p = F * p * F^T  * a
-        F = np.array(([1, dt], [0, 1]))  # state transition matrix
-        new_x = F.dot(self._x)  # how to multiply matrices
 
-        G = np.array([0.5 * dt**2, dt]).reshape(
-            (2, 1)
-        )  # this is the control input matrix
+        # state transition matrix
+        F = np.array(([1, dt], [0, 1]))
+
+        # calculate the new state by multiplying state transition matrix with current x
+        new_x = F.dot(self._x)
+
+        # G - 2x1 matrix representing the control input (models affect of acceleration on position and velocity)
+        # acceleration is assumed to be a random variable with a variance of accel_variance
+        G = np.array([0.5 * dt**2, dt]).reshape((2, 1))
+        # update covariance matrix (representing uncertainty in the state)
         new_P = F.dot(self._P).dot(F.T) + G.dot(G.T) * self._accel_variance
 
         self._P = new_P
         self._x = new_x
-        
+
     @property
     def covariance(self) -> np.ndarray:
         """The covariance of the current state vector"""
         return self._P
-    
+
     @property
     def mean(self) -> np.ndarray:
         """The mean of the current state vector"""
