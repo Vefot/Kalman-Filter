5db9041f0f377369a6388a5bd9cc3844a3ba044a
Vefot <benjamin200@hotmail.co.uk>
2023-10-06 17:08:39 +0100

    predict function and more properties

--- kf.py
+++ kf.py
@@ -9,20 +9,49 @@ class KF:
     initial_v: initial velocity
     """
 
-    def __init__(self, initial_x: float, initial_v: float) -> None:
+    def __init__(
+        self, initial_x: float, initial_v: float, accel_variance: float
+    ) -> None:
         # mean of state gaussian random variable
-        self.x = np.array([initial_x, initial_v])
+        self._x = np.array([initial_x, initial_v])
+        self._accel_variance = accel_variance
 
         # covariance of state gaussian random variable
-        self.p = np.eye(2)
+        self._P = np.eye(2)  # eye returns a 2x2 identity matrix
+
+    def predict(self, dt: float) -> None:
+        """
+        Predicts the state vector forward in time by dt (delta time) seconds
+        """
+        # x = F * x
+        # p = F * p * F^T  * a
+        F = np.array(([1, dt], [0, 1]))  # state transition matrix
+        new_x = F.dot(self._x)  # how to multiply matrices
+
+        G = np.array([0.5 * dt**2, dt]).reshape(
+            (2, 1)
+        )  # this is the control input matrix
+        new_P = F.dot(self._P).dot(F.T) + G.dot(G.T) * self._accel_variance
+
+        self._P = new_P
+        self._x = new_x
+        
+    @property
+    def covariance(self) -> np.ndarray:
+        """The covariance of the current state vector"""
+        return self._P
+    
+    @property
+    def mean(self) -> np.ndarray:
+        """The mean of the current state vector"""
+        return self._x
 
     @property
     def pos(self) -> float:
-        return self.x[0]
+        """The current position"""
+        return self._x[0]
 
     @property
     def vel(self) -> float:
-        return self.x[1]
-
-
-kf = KF(initial_x=0.2, initial_v=0.5)
+        """The current velocity"""
+        return self._x[1]
--- test_kf.py
+++ test_kf.py
@@ -10,10 +10,22 @@ class TestKF(TestCase):
         """
         x = 0.2
         v = 2.3
-        kf = KF(initial_x=x, initial_v=v)
+        kf = KF(initial_x=x, initial_v=v, accel_variance=1.2)
 
         # test that the first element of the state vector is the initial x
         self.assertAlmostEqual(kf.pos, x)
 
         # test that the second element of the state vector is the initial v
         self.assertAlmostEqual(kf.vel, v)
+
+    def test_after_calling_predict_x_and_p_are_right_shape(self):
+        x = 0.2
+        v = 2.3
+        kf = KF(initial_x=x, initial_v=v, accel_variance=1.2)
+        kf.predict(dt=0.1)
+
+        # the covariance matrix should be 2x2
+        self.assertEqual(kf.covariance.shape, (2, 2))
+
+        # the state vector should be 2x1
+        self.assertEqual(kf.mean.shape, (2,))
